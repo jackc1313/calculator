db4213fc6de28a8e7eba132beb4ee66e680d90da
Jacopo Cariato <jacopo.cariato98gmail.com>
2024-09-28 16:24:36 +0200

    added functions for the main calc operations

--- brain.js
+++ brain.js
@@ -1,5 +1,11 @@
-function add(a, b) {
-    return a + b;
-}
+let add = (a, b) => a + b;
+console.log(add(3, 4));
 
-console.log(add(2, 3));
+let subtract = (a, b) => a - b;
+console.log(subtract(5, 2));
+
+let multiply = (a, b) => a * b;
+console.log(multiply(6, 8));
+
+let divide = (a, b) => a / b;
+console.log(divide(12, 3));
